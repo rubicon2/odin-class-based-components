0fe517336ae967cdf060f596b317e8c8391c9efe
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-05-03 10:22:07 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import FunctionalInput from './components/FunctionalInput';
+import ClassInput from './components/ClassInput';
+import './style.css';
+
+export default function App() {
+  return (
+    <>
+      <FunctionalInput name="Functional component!" />
+      <div className="divider" />
+      <ClassInput name="Class based component!" />
+    </>
+  );
+}
